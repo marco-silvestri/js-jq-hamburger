3427ca1382cec5010fff45abfb02385c72fe4f76
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-08 15:39:10 +0200

    comments

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 
+// References
 var hamburgerOpen = $('.header-right a');
 var hamburgerClose = $('.close');
 var hamburgerMenu = $('.hamburger-menu');
 
+// Open ze menu
 hamburgerOpen.click(function () { 
     hamburgerMenu.css('display', 'block');
 });
 
+
+// Close ze menu
 hamburgerClose.click(function () { 
     hamburgerMenu.css('display', 'none');
 });
