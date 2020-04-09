55918a00506b69016720a2d50698f2674c7be58a
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-09 09:47:54 +0200

    Class correction

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 
 // References
-var hamburgerOpen = $('.header-right a');
+var hamburgerOpen = $('.header-right > a');
 var hamburgerClose = $('.close');
 var hamburgerMenu = $('.hamburger-menu');
 
+/*
 // Open ze menu
 hamburgerOpen.click(function () { 
     hamburgerMenu.css('display', 'block');
 });
 
-
 // Close ze menu
 hamburgerClose.click(function () { 
     hamburgerMenu.css('display', 'none');
+});
+*/
+
+// Open ze menu
+hamburgerOpen.click(function () { 
+    hamburgerMenu.addClass('active');
+});
+
+// Close ze menu
+hamburgerClose.click(function () { 
+    hamburgerMenu.removeClass('active');
 });
